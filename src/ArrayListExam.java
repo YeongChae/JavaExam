3c6e511ade0f14b24616f87bae875a1077c9a438
YeongChae Kim <neocherry83@gmail.com>
2016-01-25 14:52:04 +0900

    update160125

--- src/ArrayListExam.java
+++ src/ArrayListExam.java
@@ -7,23 +7,32 @@ public class ArrayListExam {
     public static void main(String[] args) {
         Picture pic1 = new Picture("degas1.jpg");
         Picture pic2 = new Picture("gaugin1.jpg");
-        Picture pic3 = new Picture("i11428746022.gif");
-        Picture pic4 = new Picture("monet1.jpg");
-        Picture pic5 = new Picture("monet2.jpg");
+        Picture pic3 = new Picture("monet1.jpg");
+        Picture pic4 = new Picture("monet2.jpg");
+        Picture pic5 = new Picture("renoir1.jpg");
 
         // ArrayList<Picture> 생성하고 add하고
         ArrayList<Picture> list = new ArrayList<>();
+
         list.add(pic1);
         list.add(pic2);
         list.add(pic3);
         list.add(pic4);
         list.add(pic5);
         // for문으로 draw
+        int width = 0;
+        int widthSum = 0;
+        int count = 0;
+
 
 //        for (int i = 0; i < list.size(); i++) {
 //            Picture pic = list.get(i);
+//            pic.translate(width, 0);              // 최초 0 , 0 에서 그림이 그려지고,
+//            width += pic.getWidth() + 30;         // +=로 width 에 30을 계속 더하면 for문 반복하면서 그곳에서 시작.
 //            pic.draw();
+//            widthSum += pic.getWidth();
 //        }
+//        System.out.println(widthSum);           //가로 길이의 총합
         // for 앞에 알트 + 엔터 누르고 향상된 for문으로 자동변환가능
 //        for (Picture pic : list) {
 //            pic.draw();
@@ -33,9 +42,34 @@ public class ArrayListExam {
         // pic1.getWidth() : 가로 길이
         // pic1.translate(x,y) : x,y 만큼 그림 이동
         // pic1.draw()  : 그림을 출력
-        System.out.println(pic1.getMaxX());
-        pic1.translate(100, 0);
-        pic1.draw();
+//        System.out.println(pic1.getMaxX());
+//        pic1.translate(100, 0);
+//        pic1.draw();
+
+
+         // 세로사진만 출력
+        ArrayList<Picture> matches = new ArrayList<>();
+        for (int i = 0; i < list.size() ; i++) {
+            Picture pic = list.get(i);
+            if (pic.getWidth() < pic.getHeight()) {
+                matches.add(pic);
+                count++;                          // if문에 들어왔을때만 카운팅.
+            pic.translate(width, 0);              // 최초 0 , 0 에서 그림이 그려지고,
+            width += pic.getWidth() + 30;         // +=로 width 에 30을 계속 더하면 for문 반복하면서 그곳에서 시작.
+                pic.draw();
+                break; //세로 사진의 첫번째 사진 출력  (for문을 break로 1회 draw 후 종료)
+
+            }
+
+
+        }
+        System.out.println(count);
+        matches.get(0).draw();
+        matches.get(1).draw();
+        matches.get(2).draw();
+
+
+        //세로 사진만 찾아서 matches(ArrayList)에 저장
 
     }
 }
